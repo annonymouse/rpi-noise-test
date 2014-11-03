3dda78447187039d8990ceab0c0ada69afb76d9b
annonymouse <annonymouse@example.co.uk>
2014-11-03 00:10:10 +0000

    Refactor shorter so that it's a bit clearer what's going on.  Also only register on the edge i care about, and deregister others

--- shorter.py
+++ shorter.py
@@ -1,59 +1,56 @@
 #!/usr/bin/env python
 
-import argparse, subprocess
+import argparse, subprocess, os
 import RPi.GPIO as GPIO
 from time import sleep
 
 def _resp(port, programs):
     pids = dict()
-    def callback(gpio):
-        if gpio != port:
-            return
-        state = GPIO.input(gpio)
-        if state == 1:
-            # Stop doing things
-            print "Changed to 1"
-            for prog in programs:
-                if pids and prog in pids:
-                    pid = pids[prog]
-                    pid.poll()
-                    if not pid.returncode:
-                        pid.kill()
-                    else:
-                        print "{0} not running {1}".format(prog, pid.returncode)
+    def kill():
+        print "kill"
+        GPIO.remove_event_detect(port)
+        GPIO.add_event_detect(port, GPIO.FALLING, callback=lambda _x: start(),
+                bouncetime=500)
+        for prog in programs:
+            if prog in pids:
+                pids[prog].poll()
+                if not pids[prog].returncode:
+                    pids[prog].kill()
                 else:
-                    print "{0} hasn't been started, we can't stop it"
-        else:
-            # Start doing things
-            print "Changed to 0"
-            for prog in programs:
-                if prog in pids:
-                    pid = pids[prog]
-                    pid.poll()
-                    if pid.returncode:
-                        pid= subprocess.Popen(prog)
-                    else:
-                        print "program is still running - we can't restart"
+                    print "{0} not running {1}".format(prog, pids[prog].returncode)
+            else:
+                print "{0} hasn't been started, we can't stop it"
+    def start():
+        print "start"
+        GPIO.remove_event_detect(port)
+        GPIO.add_event_detect(port, GPIO.RISING, callback=lambda _x: kill(),
+                bouncetime=500)
+        for prog in programs:
+            if prog in pids:
+                pids[prog].poll()
+                if pids[prog].returncode:
+                    pids[prog]= subprocess.Popen(prog)
+                else:
+                    print "program is still running - we can't restart"
+            else:
+                pids[prog] = subprocess.Popen(prog)
 
-    if 0 == GPIO.input(port):
-        print "port shorted"
-        for p in programs:
-            pids[p] = subprocess.Popen(p)
+    state = GPIO.input(port)
+    if state == 1:
+        # Stop doing things
+        print "Not shorted"
+        kill()
     else:
-        print "not shorted"
-
-        GPIO.add_event_detect(port, GPIO.BOTH,
-            callback=callback, bouncetime=100)
-
+        print "Shorted"
+        start()
     while True:
         sleep(5)
     return
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-            help="Will run a list of programs when the port pin is pulled down")
+    parser = argparse.ArgumentParser()
     # For now only allow pin 26
-    parser.add_argument("port", type=int, choices=(26),
+    parser.add_argument("port", type=int, choices=[26],
             help="GPIO port to detect a short on")
     parser.add_argument("program", help="Program to execute when gpio fires",
             nargs="+")
